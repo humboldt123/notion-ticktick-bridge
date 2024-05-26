421c53f884168ebf63f47ee5f8c349610864f216
Vish <vvm33@drexel.edu>
2024-05-26 16:01:28 -0400

    feat: express setup

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("omg")
+var express = require("express");
+var request = require("request");
+var cors = require("cors");
+var querystring = require("querystring");
+var cookieParser = require("cookie-parser");
+
+var app = express()
+    .use(express.static(__dirname + "/public"))
+    .use(cors())
+    .use(cookieParser());
+
+var auth = require("./routes/auth.js");
+app.use("/auth", auth);
+
+app.get("/", (request, response) => {
+    // serve landing page ✨
+    response.sendFile(__dirname + "/public/index.html");
+});
+
+// listen for requests
+const listener = app.listen(process.env.PORT, () => {
+    console.log("Your app is listening on port " + listener.address().port);
+});  
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var request = require('request');
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+const auth = express.Router()
+
+module.exports = auth;
